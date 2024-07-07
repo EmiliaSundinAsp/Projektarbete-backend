38d31cf61dff04e173d270a319b5fd96ab788dd2
EmiliaSundinAsp <emiliasundinasp@gmail.com>
2024-07-07 22:55:03 +0200

    Add MongoDB to the project

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('Hello world!');
+const express = require('express');
+const mongoose = require('mongoose');
+
+const connectString = 'mongodb+srv://emiliasundinasp:fågel-fisk-mittimellan@backend-chat-app.mkvm9ci.mongodb.net/chat-api?retryWrites=true&w=majority';
+
+const app = express(); // Skapa en express-applikation
+
+app.get('/', function (req, res) {
+	res.send('Hello World')
+})
+
+// Connect to MongoDB database using Mongoose ODM (Object Data Modeling) library for MongoDB and Node.js
+mongoose.connect(connectString)
+	.then(() => {
+		// Connection successful
+		console.log('Connected to database!')
+		// Start server on port 3000 after successful connection to database
+		app.listen(3000, () => {
+			console.log('Server is running on port 3000!');
+		})
+})
+.catch(() => {
+	// Connection failed
+	console.log('Database connection failed!')
+});
--- project.md
+++ project.md
@@ -1,5 +1,7 @@
-För att starta:
+# Projektanteckningar
 
-- Node.js
-- MongoDB
-- Express
+## För att starta:
+
+- Node.js för att kunna köra javascript utanför webbläsaren.
+- MongoDB för att spara data.
+- Express för att kunna ta emot och skicka data via http protokollet.
